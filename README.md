46e1c52d650b9f6faea2c30350d1f6f79b4cac0e
Eric Abouaf <eric.abouaf@gmail.com>
2012-01-17 22:18:42 +0100

    Commenting Yahoo Pipes import (not working and not in target release)

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ Visual scripting for the web, Pipes for Hackers, using Node.js and mongoDB
 
 ## Requirements
 
- * [Node.js](http://nodejs.org/) (>= 0.4.0)
+ * [Node.js](http://nodejs.org/) (>= 0.6.6)
  * [npm](http://npmjs.org/)
- * [mongoDB](http://www.mongodb.org/)
+ * [mongoDB](http://www.mongodb.org/) (>= 2)
 
 ## Install
 
     git clone git://github.com/neyric/webhookit.git
     cd webhookit
-    npm link .
+    npm install .
     
 ## Run
 
@@ -27,7 +27,7 @@ Visual scripting for the web, Pipes for Hackers, using Node.js and mongoDB
  * launch the webhookit server
  
     cd /path/to/webhookit
-    ./server.js
+    node server.js
     
  * open browser to http://localhost:8124
 
--- server.js
+++ server.js
@@ -7,11 +7,11 @@
  * 	- load controllers & helpers
  */
 var express = require('express'),
-	 fs = require('fs'),
-	 Db = require('mongodb').Db,
-    Server = require('mongodb').Server,
-    connect = require('connect'),
-    mongoStore = require('connect-mongodb');
+	fs = require('fs'),
+	Db = require('mongodb').Db,
+	Server = require('mongodb').Server,
+	connect = require('connect'),
+	mongoStore = require('connect-mongodb');
 
 // express server
 var app = express.createServer();
@@ -26,38 +26,45 @@ app.db = new Db(app.config.database.db,
 			);
 console.log("Connecting to MongoDB...");
 
-app.db.open(function() {
+app.db.open(function(err) {
+	
+	// Don't start without MongoDB running
+	if(err) {
+		console.log(err);
+		return;
+	}
 	
 	// Middleware setup
 	app.use(express.logger());				// Enable request logging	
 	app.use(express.bodyParser());		// parses urlencoded request bodies which populates req.body
 	app.use(express.methodOverride());	// sets a hidden input of _method to an arbitrary HTTP method 
 	app.use(express.cookieParser());	// Required by session
-
+	
 	// Use connect-mongodb SessionStore
 	app.use( connect.session({
-       cookie: {maxAge: 60000 * 20}, // 20 minutes
-       secret: app.config.sessions.secret,
-       store: new mongoStore({db: app.db})
-   }));
-
+		cookie: {maxAge: 60000 * 20}, // 20 minutes
+		secret: app.config.sessions.secret,
+		store: new mongoStore({db: app.db})
+		})
+	);
+	
 	// User model
 	User = require(app.root + '/lib/user').init(app.db);
-
+	
 	// Authentication => populate req.current_user
 	app.use(
 		(function() {
 			return function(req, res, next) {
-			    User.authenticate(req, res, function(current_user) {
+				User.authenticate(req, res, function(current_user) {
 					req.current_user = current_user;
-			    	next();
+					next();
 				}, function() {
 				    next();
 				});
 			};
 		})()
 	);
-
+	
 	app.dynamicHelpers({
 		 // req.flash to html helper
 		 messages: function(req, res){
@@ -83,26 +90,25 @@ app.db.open(function() {
 		        buf.push('</div>');
 		        return buf.join('\n');
 		    };
-		},		
-	    current_user: function(req, res){
-	        return req.current_user;
-	    }
+		},
+		current_user: function(req, res){
+			return req.current_user;
+		}
 	});
-
+	
 	app.use(app.router);
 	app.use(express["static"](app.root + '/public'));
-
+	
 	app.set('views', app.root + '/lib/views');
 	app.set('view engine', 'ejs');
-
 	
 	app.error(function(err, req, res, next){
-	    if (err.message == "Unauthorized") {
+		if (err.message == "Unauthorized") {
 			res.render('401', { status: 401, locals: {	title: 'Unauthorized', action: 'error', error: err }});
-	    }
-		 else {
+		}
+		else {
 			res.render('500', { status: 500, locals: {	title: 'Error', action: 'error', error: err }});
-	    }
+		}
 	});
 	
 	
@@ -135,7 +141,7 @@ app.db.open(function() {
 
 	// Intercept exceptions after init so that the server doesn't crash at each uncatched exception
 	process.on('uncaughtException', function (err) {
-  		console.log('Caught exception: ' + err);
+		console.log('Caught exception: ' + err);
 		console.log(err.stack);
 	});
 	
