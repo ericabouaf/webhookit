09efe390767a128975565f3f86c304498ccc0c58
Eric Abouaf <eric.abouaf@gmail.com>
2012-01-19 01:12:24 +0100

    Starting yui2->yui3 replacement

--- public/javascripts/webhookit/template.js
+++
@@ -1,9 +0,0 @@
-
-var editor = CodeMirror.fromTextArea('code', {
-  height: "350px",
-  parserfile: "parsexml.js",
-  stylesheet: "/javascripts/codemirror/css/xmlcolors.css",
-  path: "/javascripts/codemirror/js/",
-  continuousScanning: 500,
-  lineNumbers: true
-});
